c923dbe449d16f11b5ae4670190d952707c1eae3
erik <erik.rodrigu3z@gmail.com>
2020-08-08 04:37:55 -0600

    Add project files.

---
+++ VPModel/Models/DBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VPModel.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("MyDBConnectionString")
+        {
+
+        }
+
+        //clases de modelo       
+        public DbSet<Usuarios> usuariosDB { get; set; }
+
+    }
+}
---
+++ VetPaq/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VPModel.Models;
+using DAL.Services;
+
+namespace VetPaq.Controllers  
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //referencia al proyecto de modelo
+            List<Usuarios> listUsuario = new List<Usuarios>();
+            DAL.Services.DAL repo = new DAL.Services.DAL();
+
+            listUsuario = repo.GetReaderFromStringToList<Usuarios>("select * from Usuarios");
+            return View(listUsuario);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
